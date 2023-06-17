08af53ed59f38d63c279a2c8d7438a8364c9a2b1
devaaamee <devaaamee@gmail.com>
2023-06-17 17:10:53 +0700

    Pomodoro application

---
+++ podomoro/index.js
@@ -0,0 +1,40 @@
+const elTimer = document.getElementById("timer");
+const elStart = document.getElementById("start");
+const elStop = document.getElementById("stop");
+const elReset = document.getElementById("reset");
+
+let interval;
+let waktuTersisa = 120;
+
+function updateWaktu() {
+  let menit = Math.floor(waktuTersisa / 60);
+  let detik = waktuTersisa % 60;
+
+  elTimer.innerHTML = `${menit.toString().padStart(2, "0")}:${detik
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+elStart.addEventListener("click", () => {
+  interval = setInterval(() => {
+    waktuTersisa--;
+    updateWaktu();
+
+    if (waktuTersisa < 0) {
+      clearInterval(interval);
+      alert("Istirahat dulu bos");
+      waktuTersisa = 120;
+      updateWaktu();
+    }
+  }, 1000);
+});
+
+elStop.addEventListener("click", () => {
+  clearInterval(interval);
+});
+
+elReset.addEventListener("click", () => {
+  clearInterval(interval);
+  waktuTersisa = 120;
+  updateWaktu();
+});
