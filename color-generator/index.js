2235b4cbd482429d5bcaffc7c56b0866225bf39c
devaaamee <devaaamee@gmail.com>
2023-07-04 14:24:11 +0700

    Adding copy clipboard feature

--- color-generator/index.js
+++ color-generator/index.js
@@ -1,18 +1,4 @@
-const elColorContainers = document.querySelectorAll(".color-container");
-
-const elButton = document.getElementById("btn");
-elButton.addEventListener("click", () => {
-  generate();
-});
-
-function generate() {
-  for (let i = 0; i < elColorContainers.length; i++) {
-    const element = elColorContainers[i];
-
-    element.style.background = generateRandomColor();
-  }
-}
-
+// function for generate random hexa value of color
 function generateRandomColor() {
   const colorChar = "abcdef0123456789";
   const colorLength = 6;
@@ -25,5 +11,39 @@ function generateRandomColor() {
   return res;
 }
 
-let tes = generateRandomColor();
-console.log(tes);
+let colorValue;
+const elColorContainers = document.querySelectorAll(".color-container");
+function generate() {
+  for (let i = 0; i < elColorContainers.length; i++) {
+    const element = elColorContainers[i];
+    colorValue = generateRandomColor();
+
+    element.style.background = colorValue;
+    element.setAttribute("data-color", colorValue);
+  }
+}
+
+const elButton = document.getElementById("btn");
+elButton.addEventListener("click", () => {
+  generate();
+});
+
+const copyContent = async (color) => {
+  try {
+    await navigator.clipboard.writeText(color);
+    if (color === "") {
+      alert("Warna belum ada");
+    } else {
+      alert(`Warna ${color} telah dicopy ke clipboard`);
+    }
+  } catch (err) {
+    alert("Failed to copy: ", err);
+  }
+};
+
+for (let i = 0; i < elColorContainers.length; i++) {
+  const element = elColorContainers[i];
+  element.addEventListener("click", function () {
+    copyContent(element.getAttribute("data-color"));
+  });
+}
